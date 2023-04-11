ce4e96908f99d3a7e7cc07e9324d5af7d6db7b37
sodaloser <sodaloser@Ubuntu.myguest.virtualbox.org>
2023-04-11 14:47:05 +0200

    matplotlib fixed

--- visu_velo_web.py
+++ visu_velo_web.py
@@ -1,3 +1,5 @@
 from web import create_app
 
+
 app = create_app()
+
--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,9 @@
 import datetime
-
-from flask import Flask
+import io
+import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+from flask import Flask, render_template
 
 
 def create_app():
@@ -10,5 +13,37 @@ def create_app():
     def home():
         time = datetime.datetime.now()
         return f"Hello user at {time}"
+
+    @app.route('/velo')
+    def velo():
+        fichier = './parcours.csv'
+        df = pd.read_csv(fichier, sep=';')
+        df['date'] = pd.to_datetime(df['date'], dayfirst=True)
+        x1 = df['date']
+        y1 = df['km']
+        y2 = df['Deniv']
+
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+
+        ax2.plot(x1, y2, 'g-')
+        ax2.set_ylabel('Dénivelé (m)', color='g')
+
+        ax1.bar(x1, y1, width=0.5)
+        ax1.set_ylabel('Km parcourus', color='b')
+
+        plt.xlabel('Date')
+        ax1.grid(color='#95a5a6', linestyle='--', linewidth=1, axis='y', alpha=0.7)
+
+        # ax1.set_xticklabels(df['date'],rotation=90)
+        fig.autofmt_xdate()
+
+        png_image = io.BytesIO()
+        fig.savefig(png_image, format='png')
+        png_image.seek(0)
+
+        png64 = base64.b64encode(png_image.read()).decode('ascii')
+
+        return render_template('velo.html', image=png64)
 
     return app
